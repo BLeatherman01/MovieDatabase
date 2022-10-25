1cc9b8a4238954ca79daa4cb72030471b35bf4c9
brandon <bleatherman01@gmail.com>
2022-10-25 18:38:59 -0400

    First commit

---
+++ MovieList.cs
@@ -0,0 +1,52 @@
+﻿using MovieDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace MovieDatabase
+{
+    public class MovieList
+    {
+    
+      public MovieContext db { get; set; } = new MovieContext();
+
+        public void SearchByTitle()
+        {
+            List<Movie> movieListByTitle = new List<Movie>();
+            Console.WriteLine("What movie title would you like to select?");
+            string movieSelection = Console.ReadLine();
+            movieListByTitle = db.Movies.Where(m => m.Title.Contains(movieSelection)).ToList();
+            foreach(Movie movie in movieListByTitle)
+            {
+                Console.WriteLine(movie.Title + " - " + movie.Genre + " - Runtime in minutes: " + movie.Runtime );
+            }
+          
+        }
+
+        public void SearchByGenre()
+        {
+            List<Movie> movieListByGenre = new List<Movie>();
+            Console.WriteLine("What movie genre would you like to select?");
+            string movieSelection = Console.ReadLine();
+            movieListByGenre = db.Movies.Where(m => m.Genre.Contains(movieSelection)).ToList();
+            foreach (Movie movie in movieListByGenre)
+            {
+                Console.WriteLine(movie.Title + " - " + movie.Genre + " - Runtime in minutes: " + movie.Runtime);
+            }
+        }
+
+        public void PrintMoviesByTitle(List<Movie> movies)
+        {
+            Console.WriteLine("Print Movie titles:");
+            foreach (Movie movie in movies)
+            {
+                Console.WriteLine(movie.Id + " " + movie.Title + " - " + movie.Genre);
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using MovieDatabase.Models;
+
+namespace MovieDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieContext db = new MovieContext();
+            List<Movie> m = db.Movies.ToList();
+            MovieList ml  = new MovieList();
+
+           
+           
+            bool askAgain = true;
+            while(askAgain)
+            {
+                ml.PrintMoviesByTitle(m);
+                Console.WriteLine("Would you like to search by Title or Genre?");
+                string input = Console.ReadLine().ToLower();  
+                if(input == "title")
+                {
+                    ml.SearchByTitle();
+                }
+                else if(input == "genre")
+                {
+                    ml.SearchByGenre();
+                }
+
+                askAgain = GoAgain();
+            }
+        }
+        public static bool GoAgain()
+        {
+            Console.WriteLine("\nDo you want to find another movie? Please enter Yes or No");
+            string userInput = Console.ReadLine().ToLower().Trim();
+
+            if (userInput == "yes" || userInput == "y")
+            {
+
+                return true;
+            }
+            else if (userInput == "no" || userInput == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Please enter Yes or No");
+                return GoAgain();
+            }
+        }
+    }
+}
